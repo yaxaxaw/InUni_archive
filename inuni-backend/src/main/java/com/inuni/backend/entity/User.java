09ee665e82b04a382a2858b51aec106af08e857f
Nurdoolot <nzhoodarov@gmail.com>
2026-05-20 18:03:36 +0600

    feat: Add User entity and UserRepository

---
+++ inuni-backend/src/main/java/com/inuni/backend/entity/User.java
@@ -0,0 +1,31 @@
+package com.inuni.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+}
+
