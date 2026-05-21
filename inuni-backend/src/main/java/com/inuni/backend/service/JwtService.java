43bff859187b961e2e385191913fb8443a7e6658
Azimbai <azoodarov@gmail.com>
2026-05-21 09:37:50 +0600

    feat: fix service

--- inuni-backend/src/main/java/com/inuni/backend/service/JwtService.java
+++ inuni-backend/src/main/java/com/inuni/backend/service/JwtService.java
@@ -26,7 +26,7 @@ public class JwtService {
         return Jwts.builder()
                 .subject(email)
                 .issuedAt(new Date())
-                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
+                .expiration(new Date(System.currentTimeMillis() + 1000L * 60 * 60 * 24 * 7))
                 .signWith(key)
                 .compact();
     }
